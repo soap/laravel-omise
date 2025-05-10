30edccf2024416cf015bff1c0288069f73044fd6
Prasit Gebsaap <prasit.gebsaap@outlook.com>
2025-05-11 00:28:39 +0700

    Add balance object

---
+++ src/Omise/Balance.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Soap\LaravelOmise\Omise;
+
+use Soap\LaravelOmise\Omise\Helpers\OmiseMoney;
+
+class Balance extends BaseObject
+{
+    private $omiseConfig;
+
+    public function __construct(OmiseConfig $omiseConfig)
+    {
+        $this->omiseConfig = $omiseConfig;
+    }
+
+    /**
+     * Retrieve balance information
+     *
+     * @return \Soap\LaravelOmise\Omise\Error|self
+     */
+    public function retrieve()
+    {
+        try {
+            $this->refresh(OmiseBalance::retrieve($this->omiseConfig->getPublicKey(), $this->omiseConfig->getSecretKey()));
+        } catch (\Exception $e) {
+            return new Error([
+                'code' => 'not_found',
+                'message' => $e->getMessage(),
+            ]);
+        }
+
+        return $this;
+    }
+
+    public function getTransferableAmount()
+    {
+        return OmiseMoney::toCurrencyUnit($this->transferable, $this->currency);    
+    }
+
+    public function getReservedAmount()
+    {
+        return OmiseMoney::toCurrencyUnit($this->reserved, $this->currency);
+    }
+
+    public function getTotalAmount()
+    {
+        return OmiseMoney::toCurrencyUnit($this->total, $this->currency);
+    }
+}
